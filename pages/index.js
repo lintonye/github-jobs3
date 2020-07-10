555abf81b28c457f46cc4928d2a6cc4a43ab061a
Linton Ye <linton@jimulabs.com>
2020-07-09 17:04:37 -0700

    Add filters implementation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
-import HelloWorld from '../components'
-
-export default function Home() {
+function Header() {
   return (
-    <div>
-      <h1>Simple Storybook Example</h1>
-      <HelloWorld />
+    <div className="header">
+      Github Jobs
+      <form className="search-form">
+        <input type="search" name="" />
+        <button>Search</button>
+      </form>
     </div>
-  )
+  );
+}
+function Filters() {
+  const locations = ["London", "Amsterdam", "New York", "Berlin"];
+  return (
+    <div className="filters">
+      <label>
+        <input type="checkbox" />
+        Full time
+      </label>
+      <label>
+        LOCATION
+        <input type="search" />
+      </label>
+      <fieldset>
+        {locations.map((location) => (
+          <label key={location}>
+            <input type="radio" />
+            {location}
+          </label>
+        ))}
+      </fieldset>
+    </div>
+  );
+}
+function Results() {
+  return <div>Results</div>;
+}
+function JobList() {
+  return (
+    <div className="job-list">
+      <Header />
+      <Filters />
+      <Results />
+    </div>
+  );
+}
+export default function Home() {
+  return <JobList />;
 }
